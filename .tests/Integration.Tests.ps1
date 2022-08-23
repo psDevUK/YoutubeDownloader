62db364782e5ae6b798ba1df552027c81b33eb82
Adam <adambacon1@hotmail.com>
2022-08-23 18:58:02 +0100

    Update Integration.Tests.ps1

--- .tests/Integration.Tests.ps1
+++ .tests/Integration.Tests.ps1
@@ -1,1 +1,12 @@
-
+Describe "Integration Tests" -Tag "IntegrationTests" {
+    BeforeAll {
+          Import-Module ../YoutubeDownloader/YoutubeDownloader.psd1
+    }
+    Context "The command works"
+    It "returns the expected values" {
+          $null = Save-YoutubeVideo -URL "https://www.youtube.com/watch?v=0EqSXDwTq6U" -Path /tmp/
+          $video = Get-ChildItem -Path /tmp/ -Filter *.mp4 | Sort-Object LastWriteTime -Descending | Select-Object -First 1
+          $video.FullName | Should -Be "/tmp/Charlie bit my finger! ORIGINAL.mp4"
+          $video.Length | Should -BeGreaterThan 0
+    }
+}
